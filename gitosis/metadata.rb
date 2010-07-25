118e1c97a2c33b776e9283ba70297a29e84b069b
Paul McKellar <paul.mckellar@gmail.com>
2010-07-25 01:10:19 -0700

    update cookbooks for wren

---
+++ gitosis/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Heavy Water Software Inc."
+maintainer_email  "darrin@heavywater.ca"
+license           "MIT"
+description       "Installs gitosis"
+
+supports "ubuntu"
+
--- vagrant-main/recipes/default.rb
+++ vagrant-main/recipes/default.rb
@@ -1,1 +1,15 @@
-require_recipe "mysql"
+#require_recipe "mysql:server"
+#require_recipe "rails"
+
+link "/etc/nginx/sites-enabled/001-wren.conf" do
+  to "/vagrant/config/nginx/dev.conf"
+end
+
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+execute "install xml packages" do
+  command "sudo apt-get install libxml2 libxml2-dev libxslt1-dev"
+end
