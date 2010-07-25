aaa6661e8b3c0c3cd8b60e454d51d26b928e07f1
Paul McKellar <paul.mckellar@gmail.com>
2010-07-25 12:51:40 -0700

    more notes, curl

--- vagrant-main/recipes/default.rb
+++ vagrant-main/recipes/default.rb
@@ -5,11 +5,17 @@ link "/etc/nginx/sites-enabled/001-wren.conf" do
   to "/vagrant/config/nginx/dev.conf"
 end
 
-service "nginx" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start ]
+execute "install xml packages" do
+  command "sudo apt-get -y install libxml2 libxml2-dev libxslt1-dev curl"
 end
 
-execute "install xml packages" do
-  command "sudo apt-get install libxml2 libxml2-dev libxslt1-dev"
-end
+# restart nginx
+# start node
+
+# rake gems:install
+# rake db:create db:migrate
+
+#127.0.0.1       wren.dev
+#127.0.0.1       blank.wren.dev
+#127.0.0.1       www.wren.dev
+
