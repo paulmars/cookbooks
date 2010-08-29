e8e083cf65128a08b108a95dc00b7d170fe9e20f
Paul McKellar <paul.mckellar@gmail.com>
2010-08-28 22:14:44 -0700

    some changes in notes

--- vagrant-main/recipes/default.rb
+++ vagrant-main/recipes/default.rb
@@ -1,10 +1,9 @@
 execute "link up manage" do
   command "ln -s /vagrant/config/nginx/dev.conf /etc/nginx/conf.d/manage.conf"
   command "sudo /etc/init.d/nginx restart"
-#  creates "/usr/src/v8/ChangeLog"
 end
 
-excute "add gems" do
+execute "add gems" do
   cwd "/vagrant/manage/"
   command "sudo apt-get install libxml2-dev libxslt-dev"
   command "rake gems:install"
@@ -18,3 +17,6 @@ execute "setup database" do
   command "rake RAILS_ENV=development db:drop db:create db:migrate"  
 end
 
+# ln -s /vagrant/server/wren.vagrant.init.sh /etc/init.d/wren
+# chmod 755 /etc/init.d/wren
+# /etc/init.d/wren start
